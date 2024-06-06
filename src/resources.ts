0263ff50bf9724204730a52ff609ef0264db6da5
Dan-MK9 <166556625+Dan-MK9@users.noreply.github.com>
2024-06-06 17:22:42 -0300

    feat: Segunda cena finalizada

--- src/resources.ts
+++ src/resources.ts
@@ -2,11 +2,13 @@ import { ImageSource, Loader } from "excalibur";
 import sword from "./images/sword.png";
 import logo from "./images/logo.png";
 import logoV from "./images/logo-vertical.png";
+import Gamificacao from "./images/Icones-vetorizados.png";
 
 export const Resources = {
   Sword: new ImageSource(sword),
   Logo: new ImageSource(logo),
-  LogoV: new ImageSource(logoV)
+  LogoV: new ImageSource(logoV),
+  Gamificacao: new ImageSource(Gamificacao)
 } as const;
 
 export const loader = new Loader();
--- src/scenes/gamificationScene.ts
+++ src/scenes/gamificationScene.ts
@@ -1,7 +1,45 @@
-import { Color, Engine, Scene } from "excalibur";
+import { Actor, Color, Engine, FadeInOut, Scene, Transition, vec } from "excalibur";
+import { Resources } from "../resources";
 
 export class gamificationScene extends Scene {
+    textoGamifica?: HTMLElement
+
+    onTransition(direction: "in" | "out"): Transition | undefined {
+        return new FadeInOut({
+            direction: direction,
+            color: Color.Black,
+            duration: 1000
+        })
+    }
+
     onInitialize(engine: Engine<any>): void {
-        this.backgroundColor = Color.Chartreuse
+        this.backgroundColor = Color.fromHex("#403f4c")
+
+        this.textoGamifica = document.createElement("div") as HTMLElement
+        this.textoGamifica.style.opacity = "1"
+
+        let containerGame = document.querySelector(".container-game") as HTMLElement
+        containerGame.appendChild(this.textoGamifica)
+
+        this.textoGamifica.classList.add("oq-gamificacao")
+
+        this.textoGamifica.innerHTML = `<h2>O que é gamificação?</h2>
+        <p>Gamificação é a aplicação de elementos típicos de jogos em contextos não lúdicos, com o objetivo de
+            engajar e motivar indivíduos a atingir determinados objetivos. Esta abordagem se utiliza de componentes
+            como pontuação, níveis, recompensas, desafios, e feedback imediato, visando promover comportamentos
+            desejados e aumentar a participação e o comprometimento dos participantes.
+        </p>`
+
+        let actorImg = new Actor({
+            pos: vec(350, 350)
+        })
+
+        let imagemGamificacao = Resources.Gamificacao.toSprite()
+
+        imagemGamificacao.scale = vec(0.6, 0.6)
+
+        actorImg.graphics.add(imagemGamificacao)
+
+        this.add(actorImg)
     }
 }
