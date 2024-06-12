61f5351b4cf414b4306c22b357f8919453f6c53c
Dan-MK9 <166556625+Dan-MK9@users.noreply.github.com>
2024-06-12 17:18:26 -0300

    feat: Colisões concluidas

--- src/scenes/expoScene.ts
+++ src/scenes/expoScene.ts
@@ -1,4 +1,4 @@
-import { Color, Engine, FadeInOut, Resource, Scene, Transition, vec } from "excalibur";
+import { Actor, CollisionType, Color, Engine, FadeInOut, Resource, Scene, Transition, vec } from "excalibur";
 import { Resources } from "../resources";
 import { Player } from "../actors/player";
 
@@ -26,6 +26,21 @@ export class expoScene extends Scene {
         let Jogador = new Player()
 
         this.add(Jogador)
+
+        let camadaObjetosColisores = tiledMap.getObjectLayers("ObjetosColisores")[0]
+
+        camadaObjetosColisores.objects.forEach(objeto => {
+            const objetoAtual = new Actor({
+                name: objeto.name,
+                x: objeto.x + offsetX + (objeto.tiledObject.width!/2),
+                y: objeto.y + offsetY + (objeto.tiledObject.height!/2),
+                width: objeto.tiledObject.width,
+                height: objeto.tiledObject.height,
+                collisionType: CollisionType.Fixed
+            })
+
+            this.add(objetoAtual)
+        })
     }
     
 }
